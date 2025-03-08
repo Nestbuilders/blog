e7a88554ca65ef548dc6e0fa8619dc4862128bae
Gopi Dev <mrgopinath565@gmail.com>
2025-03-08 08:20:43 +0530

    third

--- action.js
+++ action.js
@@ -67,15 +67,15 @@ links[3].addEventListener("click", () => { openlink(4) });
 
 function openlink(x) {
     if (x == 1) {
-        window.open("https://www.instagram.com/_.vini._02_/", "_blank");
+        window.open("https://nestbuilders.github.io/home/", "_blank");
     }
     if (x == 2) {
-        window.open("https://www.linkedin.com/in/vineet-kumar-gupta-2833ab196/", "_blank");
+        window.open("https://nestbuilders.github.io/home/", "_blank");
     }
     if (x == 3) {
-        window.open("https://github.com/VineetKumar02", "_blank");
+        window.open("https://nestbuilders.github.io/home/", "_blank");
     }
     if (x == 4) {
-        window.open("https://vineet-portfolio-site.netlify.app/", "_blank");
+        window.open("https://nestbuilders.github.io/home/", "_blank");
     }
 }
